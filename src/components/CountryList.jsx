52b80f722e2fa5705a400e02384cb5f893232186
AhmedKhalaf <ahmed.khalaf.1088@gmail.com>
2024-09-27 01:26:44 +0200

    add city list and country list to the Sidebar in the page AppLayout

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [
+        ...arr,
+        { country: city.country, emoji: city.emoji, id: city.id },
+      ];
+    else return arr;
+  }, []);
+  console.log(countries);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+CountryList.propTypes = {
+  cities: PropTypes.array,
+  isLoading: PropTypes.bool,
+};
+
+export default CountryList;
