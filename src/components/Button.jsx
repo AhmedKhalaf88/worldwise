a4361b64d2494fc1763ab479eb4abf8657d40ed6
AhmedKhalaf <ahmed.khalaf.1088@gmail.com>
2024-09-30 19:51:42 +0200

    add city and Form component

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import styles from "./Button.module.css";
+import PropTypes from "prop-types";
+
+function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+};
+
+export default Button;
